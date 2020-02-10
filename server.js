7ba0b3fca0f90a3430f53a8ef5503c1a40c9f0b2
Pavel Kalinichenko <pavel.kalinichenko@gmail.com>
2020-02-11 00:03:26 +0200

    initiate CI

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to test task!" });
 })
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 })
