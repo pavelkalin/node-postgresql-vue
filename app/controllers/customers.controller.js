55bea5f838134b5c106a7fc68da22d30dab89780
Pavel Kalinichenko <pavel.kalinichenko@gmail.com>
2020-02-11 19:10:35 +0200

    add phone e164 verification

--- app/controllers/customers.controller.js
+++ app/controllers/customers.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models")
 const Customers = db.customers
 const Op = db.Sequelize.Op
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
 
 // Find a single Customers with an id
@@ -21,52 +22,69 @@ exports.findOne = (req, res) => {
 
 // Create and Save a new Customers
 exports.create = (req, res) => {
-  // First simple validation
-  if (!req.body.name) {
-    res.status(400).send({
-      message: "Name shouldn't be empty"
-    })
-    return
-  }
+    // First simple validation
+    if (!req.body.name) {
+        res.status(400).send({
+            message: "Name shouldn't be empty"
+        })
+        return
+    }
 
-  if (!req.body.email) {
-    res.status(400).send({
-      message: "Email shouldn't be empty"
-    })
-    return
-  }
+    if (!req.body.email) {
+        res.status(400).send({
+            message: "Email shouldn't be empty"
+        })
+        return
+    }
 
-  if (!req.body.phone) {
-    res.status(400).send({
-      message: "Email shouldn't be empty"
-    })
-    return
-  }
+    if (!req.body.phone) {
+        res.status(400).send({
+            message: "Phone shouldn't be empty"
+        })
+        return
+    }
 
-  if (!req.body.contract_number) {
-    res.status(400).send({
-      message: "Contract number shouldn't be empty"
-    })
-    return
-  }
 
-  // Create a Customers
-  const customers = {
-    name: req.body.name,
-    phone: req.body.phone,
-    email: req.body.email,
-    contract_number: req.body.contract_number,
-  }
+    // Validate for e164 format
+    try {
+        if (!phoneUtil.isValidNumber(phoneUtil.parse(req.body.phone))) {
+            res.status(400).send({
+                message: "Phone should be in e164 format"
+            })
+            return
+        }
+    } catch (e) {
+        res.status(400).send({
+            message: "Phone should be in e164 format"
+        })
+        return
+    }
 
-  // Save Customers in the database
-  Customers.create(customers)
-    .then(data => {
-      res.send(data)
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Error while creation of customer"
-      })
-    })
+
+    if (!req.body.contract_number) {
+        res.status(400).send({
+            message: "Contract number shouldn't be empty"
+        })
+        return
+    }
+
+// Create a Customers
+    const customers = {
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        contract_number: req.body.contract_number,
+    }
+
+// Save Customers in the database
+    Customers.create(customers)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Error while creation of customer"
+            })
+        })
 }
